ffbe6095b4cb63779d788dd60747cf311c8a572c
addiii4 <adityacheema4@gmail.com>
2024-04-08 21:45:07 +1000

    Add react-router-dom for multi-page routing; update package-lock.json
    The code diff above adds the "react-router-dom" package to enable multi-page routing in the application. The Navbar component and several page components have been introduced. The package-lock.json has been updated accordingly.

---
+++ src/pages/home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <section className="w-full h-screen relative">
+      <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
+        POPUP
+      </div>
+    </section>
+  )
+}
+
+export default Home
---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+import Home from './home';
+import About from './about';   
+import Projects from './projects'
+import Contact from './contact';
+
+export { Home, About, Projects, Contact } ;
