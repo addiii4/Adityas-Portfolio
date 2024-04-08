fb8e1e91fb9b0d8d69a2e345e324a9a0a5b66606
addiii4 <adityacheema4@gmail.com>
2024-04-08 17:51:37 +1000

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const app = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+};
+
+export default app;
