935411b18c177cf8bf00d3e6fcade504437e2f27
addiii4 <adityacheema4@gmail.com>
2024-04-10 16:55:42 +1000

    added plane and grab feature

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Canvas } from '@react-three/fiber';
-import { Suspense } from 'react';
+import { useState, Suspense } from 'react';
 import Loader from '../components/loader';
 import Island from '../models/island';
 import Sky from '../models/sky';
+import Plane from '../models/plane'
 
 const Home = () => {
+  const [isRotating, setisRotating] = useState(false);
+  
   const adjustIslandForScreen = () => {
     let screenScale = null;
     let screenPosition = [0, -6.5, -43];
@@ -26,21 +29,33 @@ const Home = () => {
       <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
         POPUP
       </div>
-      <Canvas className="w-full h-screen bg-transparent" camera={{ near: 0.1, far: 1000}}>
-        <Suspense fallback={<Loader/>}>
-          <directionalLight position = {[1, 1, 1]} intensity={2} />
+      <Canvas
+        className={`w-full h-screen bg-transparent ${
+          isRotating ? "cursor-grabbing" : "cursor-grab"
+        }`}
+        camera={{ near: 0.1, far: 1000 }}
+      >
+        <Suspense fallback={<Loader />}>
+          <directionalLight position={[1, 1, 1]} intensity={2} />
           <ambientLight intensity={0.5} />
-          <hemisphereLight skyColor="#b1e1ff"  groundColor='#000000' intensity= {1} />
+          <hemisphereLight
+            skyColor="#b1e1ff"
+            groundColor="#000000"
+            intensity={1}
+          />
           <Sky />
-          <Island 
+          <Island
             position={islandPosition}
             scale={islandScale}
             rotation={islandRotation}
+            isRotating={isRotating}
+            setisRotating={setisRotating}
           />
+          <Plane />
         </Suspense>
       </Canvas>
     </section>
-  )
+  );
 }
 
 export default Home
