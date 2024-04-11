c17de807cddf3e2da7b02ca6056c37d1a61bacd8
addiii4 <adityacheema4@gmail.com>
2024-04-11 21:42:12 +1000

    fixed tailwind config

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Contact = () => {
+  const [from, setfrom] = useState({name: '', email: '', message: ''})
+  const handleChange = () => {
+  
+  }
   return (
-    <div>Contact</div>
+    <section className='relative flex lg:flew-row flex-col max-container'>
+      <div className='flex-1 min-w-[50%] flex-col'>
+        <h1 className='heat-text'>Get in touch</h1>
+        <form className='w-full flex flex-col gap-7 mt-14'>
+          <label className='text-black-500 font-semibold'>
+            Name
+            <input
+              type="text"
+              name="name"
+              className='input'
+              placeholder='John'
+              required
+              value={form.name}
+              onChange={handleChange}
+            />
+          </label>
+        </form>
+      </div>
+    </section>
   )
 }
 
 export default Contact
+
