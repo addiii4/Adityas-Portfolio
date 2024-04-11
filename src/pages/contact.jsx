7ed90cae978fbc786a3f6c06c168b75be136daf4
addiii4 <adityacheema4@gmail.com>
2024-04-11 23:32:12 +1000

    Configured emails!!!

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,32 +1,132 @@
-import React, {useState} from 'react'
+import React, { useState, useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
-  const [from, setfrom] = useState({name: '', email: '', message: ''})
-  const handleChange = () => {
-  
-  }
+  const formRef = useRef(null);
+  const [form, setForm] = useState({ name: "", email: "", message: "" });
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+  //Function for when you click on text field
+  const handleFocus = () => {};
+  //Function for when you click out of text field
+  const handleBlur = () => {};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    //setCurrentAnimation("hit");
+
+    emailjs
+      .send(
+        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        {
+          from_name: form.name,
+          to_name: "Aditya",
+          from_email: form.email,
+          to_email: "adityacheema4@gmail.com",
+          message: form.message,
+        },
+        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+      )
+      .then(
+        () => {
+          setIsLoading(false);
+          // showAlert({
+          //   show: true,
+          //   text: "Thank you for your message 😃",
+          //   type: "success",
+          // });
+
+          // setTimeout(() => {
+          //   hideAlert(false);
+          //   setCurrentAnimation("idle");
+          setForm({
+            name: "",
+            email: "",
+            message: "",
+          });
+          // }, [3000]);
+        },
+        (error) => {
+          setIsLoading(false);
+          console.error(error);
+          //setCurrentAnimation("idle");
+
+          // showAlert({
+          //   show: true,
+          //   text: "I didn't receive your message 😢",
+          //   type: "danger",
+          // });
+        }
+      );
+  };
+
   return (
-    <section className='relative flex lg:flew-row flex-col max-container'>
-      <div className='flex-1 min-w-[50%] flex-col'>
-        <h1 className='heat-text'>Get in touch</h1>
-        <form className='w-full flex flex-col gap-7 mt-14'>
-          <label className='text-black-500 font-semibold'>
+    <section className="relative flex lg:flew-row flex-col max-container">
+      <div className="flex-1 min-w-[50%] flex-col">
+        <h1 className="heat-text">Get in touch</h1>
+        <form
+          ref={formRef}
+          className="w-full flex flex-col gap-7 mt-14"
+          onSubmit={handleSubmit}
+        >
+          <label className="text-black-500 font-semibold">
             Name
             <input
               type="text"
               name="name"
-              className='input'
-              placeholder='John'
+              className="input"
+              placeholder="John"
               required
               value={form.name}
               onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
             />
           </label>
+          <label>
+            Email
+            <input
+              type="email"
+              name="email"
+              className="input"
+              placeholder="john@gmail.com"
+              required
+              value={form.email}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+          <label>
+            Message
+            <textarea
+              name="message"
+              rows={4}
+              className="textarea"
+              placeholder="Write your thoughts here..."
+              value={form.message}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+          <button
+            type="submit"
+            className="btn"
+            disabled={isLoading}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+          >
+            {isLoading ? "Sending..." : "Send"}
+          </button>
         </form>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Contact
-
+export default Contact;
