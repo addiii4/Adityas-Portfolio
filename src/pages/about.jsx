bb3c3882787cffbef1d02ed3ad208061b06bdeec
addiii4 <adityacheema4@gmail.com>
2024-04-12 06:25:45 +1000

    Added education section as timeline

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -39,6 +39,62 @@ const About = () => {
         </div>
       </div>
       <div className="py-16">
+        <h3 className="subhead-text">Education</h3>
+        <div className="mt-5 flex-col gap-3 text-slate-500">
+          <p>
+            I've completed my Bachelor's of Software Engineering (Honors) from the University of Queensland.
+          </p>
+        </div>
+        <div className="mt-12 flex">
+          <VerticalTimeline>
+            {education.map((education, index) => (
+              <VerticalTimelineElement
+                key={education.company_name}
+                date={education.date}
+                iconStyle={{ background: education.iconBg }}
+                icon={
+                  <div className="flex justify-center items-center w-full h-full">
+                    <img
+                      src={education.icon}
+                      alt={education.company_name}
+                      className="w-[60%] h-[60%] object-contain"
+                    />
+                  </div>
+                }
+                contentStyle={{
+                  borderBottom: "8px",
+                  borderStyle: "solid",
+                  borderBottomColor: education.iconBg,
+                  boxShadow: "none",
+                }}
+              >
+                <div>
+                  <h3 className="text-black text-xl font-poppins font-semibold">
+                    {education.title}
+                  </h3>
+                  <p
+                    className="text-black-500 font-medium text-base"
+                    style={{ margin: 0 }}
+                  >
+                    {education.company_name}
+                  </p>
+                </div>
+                <ul className="my-5 list-disc ml-5 space-y-2">
+                  {education.points.map((point, index) => (
+                    <li
+                      key={`experience-point-${index}`}
+                      className="text-black-500/50 font-normal pl-1 text-sm"
+                    >
+                      {point}
+                    </li>
+                  ))}
+                </ul>
+              </VerticalTimelineElement>
+            ))}
+          </VerticalTimeline>
+        </div>
+      </div>
+      <div className="py-16">
         <h3 className="subhead-text">Work Experience</h3>
         <div className="mt-5 flex-col gap-3 text-slate-500">
           <p>
